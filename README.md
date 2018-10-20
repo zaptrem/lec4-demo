f9810ac5787d159b8b41f015a0e041a5f92f7ea5
Megan Le <meganle7@gmail.com>
2018-10-20 00:54:20 -0400

    Boilerplate code

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # lec4-demo
+Demo for lecture 4
+
+## Setting up development
+Follow the instructions [here](https://github.com/appdev-courses/vm) to set up a development VM.
+
+## Setup
+These are the commands to run to configure your development machine before starting each demo.
+
+    sudo pip3 install virtualenv
+    cd demo/src
+    virtualenv venv
+    . venv/bin/activate
+    pip3 install -r requirements.txt
+
+To start the server
+
+    cd demo/src
+    python3 app.py
---
+++ src/app.py
@@ -0,0 +1,59 @@
+import json
+from db import db, Task
+from flask import Flask, request
+
+db_filename = "todo.db"
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % db_filename
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+db.create_all()
+
+@app.route('/')
+@app.route('/tasks/')
+def get_tasks():
+    res = {'success': True, 'data': Db.get_all_tasks()}
+    return json.dumps(res), 200
+
+@app.route('/tasks/', methods=['POST'])
+def create_task():
+    post_body = json.loads(request.data)
+    description = post_body['description']
+    task = {
+        'id': Db.insert_task_table(description, False),
+        'description': description,
+        'done': False
+    }
+    return json.dumps({'success': True, 'data': task}), 201
+
+@app.route('/tasks/<int:task_id>/')
+def get_task(task_id):
+    task = Db.get_task_by_id(task_id)
+    if task is not None:
+        return json.dumps({'success': True, 'data': task}), 200
+    return json.dumps({'success': False, 'error': 'Task not found!'}), 404
+
+@app.route('/tasks/<int:task_id>/', methods=['POST'])
+def update_task(task_id):
+    post_body = json.loads(request.data)
+    description = post_body['description']
+    done = bool(post_body['done'])
+    Db.update_task_by_id(task_id, description, done)
+
+    task = Db.get_task_by_id(task_id)
+    if task is not None:
+        return json.dumps({'success': True, 'data': task}), 200
+    return json.dumps({'success': False, 'error': 'Task not found!'}), 404
+
+@app.route('/tasks/<int:task_id>/', methods=['DELETE'])
+def delete_task(task_id):
+    task = Db.get_task_by_id(task_id)
+    if task is not None:
+        Db.delete_task_by_id(task_id)
+        return json.dumps({'success': True, 'data': task}), 200
+    return json.dumps({'success': False, 'error': 'Task not found!'}), 404 
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
